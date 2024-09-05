efb51f8203b4d19b9d6d25f2195ff8fdabd5d829
HuuBng <89332259+HuuBng@users.noreply.github.com>
2024-09-05 16:05:23 +0700

    Change Menu to Tool

---
+++ J1.L.P0024/src/BikeStoresManagementSystem/Tool.java
@@ -0,0 +1,97 @@
+package BikeStoresManagementSystem;
+
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class Tool {
+
+    public static final Scanner sc = new Scanner(System.in);
+
+    public static int menu(ArrayList<String> opts) {
+
+        int N = opts.size();
+
+        for (int i = 0; i < N; i++) {
+            System.out.println((i + 1) + ". " + opts.get(i));
+        }
+
+        String inputString;
+        Pattern inputPattern = Pattern.compile("\\d+");
+
+        do {
+            System.out.print("Please choose an option: 1..." + N + ": ");
+            inputString = readStr();
+            if (!inputPattern.matcher(inputString).find()) {
+                System.err.println("Please enter a digit!!");
+            }
+        } while (!inputPattern.matcher(inputString).find());
+
+        return Integer.parseInt(inputString);
+    }
+
+    public static int menu(Objects... opts) {
+        int N = opts.length;
+        for (int i = 0; i < N; i++) {
+            System.out.println((i + 1) + ". " + opts[i].toString());
+        }
+
+        String inputString;
+        Pattern inputPattern = Pattern.compile("\\d+");
+
+        do {
+            System.out.print("Please choose an option: 1..." + N + ": ");
+            inputString = readStr();
+            if (!inputPattern.matcher(inputString).find()) {
+                System.err.println("Please enter a digit!!");
+            }
+        } while (!inputPattern.matcher(inputString).find());
+
+        return Integer.parseInt(inputString);
+    }
+
+    public static String readStr(String prompt) {
+        System.out.print(prompt + ": ");
+        return sc.nextLine().trim();
+    }
+
+    public static String readStr() {
+        return sc.nextLine().trim();
+    }
+
+    /**
+     * Automatically generates an increasing code.
+     *
+     * @param prefix    Ex: P
+     * @param length    Ex: 7
+     * @param curNumber Ex: 123
+     * @return Ex: P0000123
+     */
+    public static String generateCode(String prefix, int length, int curNumber) {
+        String formatStr = "%0" + length + "d";
+        return prefix + String.format(formatStr, curNumber);
+    }
+
+    /**
+     * Get number in a given code with prefix.
+     *
+     * @param code   Ex: P0000123
+     * @param prefix Ex: P0000
+     * @return Ex: 123
+     */
+    public static int getNumberInCode(String code, String prefix) {
+        return Integer.parseInt(code.substring(prefix.length()));
+    }
+
+    /**
+     * Get number in a given code with prefix length.
+     *
+     * @param code         Ex: P0000123
+     * @param prefixLength Ex: 5
+     * @return Ex: 123
+     */
+    public static int getNumberInCode(String code, int prefixLength) {
+        return Integer.parseInt(code.substring(prefixLength));
+    }
+}
