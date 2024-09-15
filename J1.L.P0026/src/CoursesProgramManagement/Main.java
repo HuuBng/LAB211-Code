444a4a86b972a47c276ff0d2fc7c6fd727715ee8
HuuBng <89332259+HuuBng@users.noreply.github.com>
2024-09-15 20:38:01 +0700

    Add Main

--- J1.L.P0026/src/CoursesProgramManagement/Main.java
+++ J1.L.P0026/src/CoursesProgramManagement/Main.java
@@ -1,4 +1,54 @@
 package CoursesProgramManagement;
 
+import java.util.ArrayList;
+
+import static CoursesProgramManagement.Tool.int_menu;
+
 public class Main {
+
+    public static void main(String[] args) {
+        Management catalog = new Management();
+
+        String dataFile = "J1.L.P0026/src/data.dat";
+        catalog.loadFromFile(dataFile);
+        ArrayList<String> menu = new ArrayList<>();
+        menu.add("Manage Topic");
+        menu.add("Manage Course");
+        menu.add("Manage Learner");
+        menu.add("Search");
+        menu.add("Save");
+        menu.add("Quit");
+
+        int choice;
+        do {
+
+            System.out.println("\n||===================================||");
+            System.out.println("||                                   ||");
+            System.out.println("|| Courses Program Management System ||");
+            System.out.println("||                                   ||");
+            System.out.println("||===================================||");
+
+            choice = int_menu(menu);
+            System.out.println();
+            switch (choice) {
+                case 1:
+                    catalog.manageTopic();
+                    break;
+                case 2:
+                    catalog.manageCourse();
+                    break;
+                case 3:
+                    catalog.manageLearner();
+                    break;
+                case 4:
+                    catalog.search();
+                    break;
+                case 5:
+                    catalog.saveToFile(dataFile);
+                    break;
+                default:
+                    System.out.println("Exiting!!!");
+            }
+        } while (choice >= 1 && choice < menu.size());
+    }
 }
